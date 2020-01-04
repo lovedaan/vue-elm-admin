b302546d004f05551085c760f8d2995ff7072dc1
赖海涛 <897955713@qq.com>
2020-01-04 11:02:50 +0800

    修改

---
+++ src/components/dot-type.vue
@@ -0,0 +1,117 @@
+<template>
+	<div class="select-wrap">
+		<el-select v-model="optionsModel" multiple @change="changeSelect" placeholder="请选择证件类型">
+			<el-option
+				v-for="item in options"
+				:key="item.id"
+				:label="item.name"
+				:value="item.id">
+			</el-option>
+		</el-select>
+		<div class="result">
+			<div class="result-item" v-for="(item, index) of selectedList" :key="index">
+				<span class="label">{{item.name}}</span>
+				<el-input @change="inputChange" v-model="item.code" style="width: 200px;" placeholder="请输入证件号码"></el-input>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: ['list'],
+		watch: {
+			list(val) {
+				this.lock = false;
+				this.handleList(val)
+			}
+		},
+		data() {
+			return {
+				selectedList: [],
+				optionsModel: [],
+				lock: true
+			}
+		},
+		mounted() {
+			this.initData();
+		},
+		methods: {
+			initData() {
+				// 接口请求证件列表数据
+				setTimeout(() => {
+					this.options = [{
+						id: '1',
+						name: '身份证'
+					}, {
+						id: '2',
+						name: '港澳通行证'
+					}, {
+						id: '3',
+						name: '台胞证'
+					}, {
+						id: '4',
+						name: '护照'
+					}, {
+						id: '5',
+						name: '学生证'
+					}];
+				}, 3000);
+			},
+			// 监听详情接口返回的已选数据
+			handleList(data) {
+				this.selectedList = [...data];
+				this.optionsModel = data.map(item => item.id);
+				this.$emit('change', this.selectedList);
+			},
+			// 监听文本框输入变化
+			inputChange(val) {
+				let t = setTimeout(() => {
+					clearTimeout(t);
+					this.$emit('change', this.selectedList);
+				}, 100)
+			},
+			// select框改变
+			changeSelect(val) {
+			 if(!this.lock) {
+					let t = setTimeout(() => {
+						this.lock = true;
+						clearTimeout(t);
+					}, 200)
+					return;
+				};
+				let ret = [];
+				this.options.forEach(item => {
+					if(val.includes(item.id)) {
+						let code = '';
+						let index1 = this.list.findIndex(r => r.id === item.id);
+						let index2 = this.selectedList.findIndex(r => r.id === item.id);
+						if(index1 >= 0 && index2 >= 0) {
+							code = this.selectedList[index1].code; 
+						}
+						ret.push({...item, code});
+					}
+				})
+				this.selectedList = ret;
+				this.$emit('change',this.selectedList);
+			}
+		}
+	}
+</script>
+
+<style lang="less" scoped="scoped">
+	.select-wrap {
+		.result {
+			margin-top: 20px;
+			.result-item {
+				display: flex;
+				align-items: center;
+				margin-bottom: 20px;
+				.label {
+					width: 80px;
+					margin-right: 20px;
+				}
+			}
+		}
+	}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,58 +1,67 @@
 <template>
-	<el-row>
-	  <el-col :span="8" v-for="(o, index) in 8" :key="o" :offset="index > 0 ? 2 : 0">
-	    <el-card :body-style="{ padding: '0px' }">
-	      <img src="@/assets/logo.png" class="image">
-	      <div style="padding: 14px;">
-	        <span>好吃的汉堡</span>
-	        <div class="bottom clearfix">
-	          <time class="time">{{ currentDate }}</time>
-	          <el-button type="text" class="button">操作按钮</el-button>
-	        </div>
-	      </div>
-	    </el-card>
-	  </el-col>
-	</el-row>
+	<div class="home-page">
+		<dotType :list="list" @change="selectChange" />
+		<div class="footer-btn">
+			<el-button type="primary" @click="submit">提交</el-button>
+		</div>
+	</div>
 </template>
 
-<style>
-  .time {
-    font-size: 13px;
-    color: #999;
-  }
-  
-  .bottom {
-    margin-top: 13px;
-    line-height: 12px;
-  }
-
-  .button {
-    padding: 0;
-    float: right;
-  }
-
-  .image {
-    width: 100%;
-    display: block;
-  }
-
-  .clearfix:before,
-  .clearfix:after {
-      display: table;
-      content: "";
-  }
-  
-  .clearfix:after {
-      clear: both
-  }
-</style>
-
 <script>
-export default {
-  data() {
-    return {
-      currentDate: new Date()
-    };
-  }
-}
+	import dotType from '@/components/dot-type.vue'
+	export default {
+		components: {
+			dotType
+		},
+		data() {
+			return {
+				list: [],
+				resList: []
+			};
+		},
+		mounted() {
+			// 模拟接口返回数据
+			setTimeout(() => {
+				this.list = [{
+					id: '1',
+					name: '身份证',
+					code: '21341244'
+				}, {
+					id: '2',
+					name: '港澳通行证',
+					code: '9999999'
+				}];
+			}, 2000)
+		},
+		methods: {
+			selectChange(res) {
+				this.resList = res;
+			},
+			isEmptyList(data) {
+				if(!data.length) return false;
+				for(let i = 0; i < data.length; i++) {
+					if(!data[i].code) {
+						return false;
+					}
+				}
+				return true;
+			},
+			submit() {
+				let isEmpty = this.isEmptyList(this.resList);
+				if(!isEmpty) {
+					this.$message.error('证件号码不能为空')
+				}
+				console.log(this.resList);
+			}
+		}
+	}
 </script>
+
+<style lang="less" scoped="scoped">
+  .home-page {
+		padding: 20px;
+		.footer-btn {
+			margin-top: 20px;
+		}
+	}
+</style>
